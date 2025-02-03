56f5f90d195373d6a5b8e97d9605aa984f658332
Bharath <premkumar@hasnatech.onmicrosoft.com>
2025-02-03 18:02:51 +0530

    component updates

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import {useParams} from 'react-router-dom';
+
+const DisplayAlbum = () => {
+  const {id} = useParams ();
+  console.log (id);
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default DisplayAlbum;
