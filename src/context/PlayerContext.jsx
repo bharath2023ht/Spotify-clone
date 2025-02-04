bed264c0247013a6c66a3b22b2e0c58aeaaaa359
Bharath <premkumar@hasnatech.onmicrosoft.com>
2025-02-04 16:16:01 +0530

    'update'

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import {createContext, useRef, useState} from 'react';
+import {createContext, useEffect, useRef, useState} from 'react';
 import {songsData} from '../assets/assets';
 
 export const PlayerContext = createContext ();
@@ -31,6 +31,36 @@ const PlayerContextProvider = props => {
     setPlayStatus (false);
   };
 
+  const playWithId = async id => {
+    await setTrack (songsData[id]);
+    await audioRef.current.play ();
+    setPlayStatus (true);
+  };
+
+  useEffect (
+    () => {
+      setTimeout (() => {
+        audioRef.current.ontimeupdate = () => {
+          seekBar.current.style.width =
+            Math.floor (
+              audioRef.current.currentTime / audioRef.current.duration * 100
+            ) + '%';
+          setTime ({
+            currentTime: {
+              second: Math.floor (audioRef.current.currentTime % 60),
+              minute: Math.floor (audioRef.current.currentTime / 60),
+            },
+            totalTime: {
+              second: Math.floor (audioRef.current.duration % 60),
+              minute: Math.floor (audioRef.current.duration / 60),
+            },
+          });
+        };
+      }, 1000);
+    },
+    [audioRef]
+  );
+
   const contextValue = {
     audioRef,
     seekBar,
@@ -43,6 +73,7 @@ const PlayerContextProvider = props => {
     setTime,
     play,
     pause,
+    playWithId
   };
 
   return (
